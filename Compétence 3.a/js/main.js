fa23491b729c1f1b61087f6cb33be9a6d41ad581
WIlliam Loyer <w.loyer@outlook.com>
2022-11-09 16:04:12 +0100

    html linked to json

--- Compétence 3.a/js/main.js
+++ Compétence 3.a/js/main.js
@@ -3,17 +3,77 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 	});
 });
-const { createApp } = Vue
           
-            createApp({
-              data() {
-                  return {
-                      products: []
-                  }
-              },
-              mounted () {
-                  axios
-                  .get('products.json')
-                  .then(response => (this.products = response.data.products))
-              } 
-            }).mount('#app')
+const { createApp } = Vue
+        
+createApp({
+  data() {
+	return {
+	  
+	  products: [ ]
+	  
+	}
+  },
+  computed: {
+totalProducts() {
+  return this.products.reduce((sum, product) => {
+	// sum += product.quantity
+	// return sum
+	return sum + product.quantity;
+  }, 0);
+  
+},
+totalPrix() {
+  return this.products.reduce((total, product) => {
+  
+	return total + product.count*product.prix;
+  }, 0);
+  
+},
+
+
+
+},
+
+  created() {
+  fetch('products.json')
+  .then(response => response.json())
+  .then(json => {
+	  this.products = json.products
+	  //this.products = json
+  });
+},
+
+
+
+}).mount('#app');
+createApp({
+  data() {
+	return {
+	  
+	  products:[]
+	  
+	}
+  
+  },
+  computed:{
+	totalPrix(){
+	  return this.products.reduce((total,product) =>{
+		  return totl+product.count*product.prix;
+	  },0);
+
+	}
+  },
+
+  created() {
+  fetch('products.json')
+  .then(response => response.json())
+  .then(json => {
+	  this.products = json.products
+	  
+  });
+},
+
+
+
+}).mount('#add');
